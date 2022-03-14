33734ff332e7e47a6d7963055800427791ca845f
shahd1995913 <shahdalkhatib95@gmail.com>
2022-03-14 19:46:07 +0300

    issue with cv2

---
+++ Face Blurring.py
@@ -0,0 +1,37 @@
+# Importing libraries
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+# A function called showimage  for showing the images
+def showimage(img):
+	plt.imshow(img, cmap="gray")
+	plt.axis('off')
+	plt.style.use('seaborn')
+	plt.show()
+
+# Reading an image using OpenCV --> OpenCV reads images by default in BGR format
+image = cv2.imread("img.jpg")
+
+# Converting BGR image into a RGB image
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# show the original image
+showimage(image)
+
+# used a haarcascades  in csv make a face detect 
+detect_face = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_alt.xml')
+result = detect_face.detectMultiScale(image, 1.2, 1) # return number
+print(result)  # saved in x y w h 
+
+# Draw rectangle around the faces which is in ( saved in x y w h ) region of interest (ROI)
+for (x, y, w, h) in result:
+	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2) # bulid rectangle 
+	roi = image[y:y+h, x:x+w]
+	# applying a gaussian blur over this new rectangle area
+	roi = cv2.GaussianBlur(roi, (23, 23), 30)
+	# put the  blurred image on original image to get final image
+	image[y:y+roi.shape[0], x:x+roi.shape[1]] = roi
+    
+# get the output
+showimage(image)
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Face-Blurring
-### an algorithm that blurs out people's faces in any given colored image without affecting the remaining parts of the image, if any face exists.
+## (Problem #1) Face Blurring: You are expected to write an algorithm that blurs out people's faces in any given colored image without affecting the remaining parts of the image if any face exists.
+### I used to solve the problem open-source library called OpenCV. also, I used  Haar Cascade frontal-face classifier is available as an XML file and is used for detecting faces in an image.
+## Steps :
+## 1. Import library 
+## 2. Read the input image using OpenCV
+## 3. Shows the image 
+## 4. Detect the face
+## 5. Draw a rectangle on the detected face
+## 6. Blur the rectangle
+## 7. show the output
+ 
 
